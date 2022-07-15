d4d7df595f8346777299955644584f34ecfff384
Mihir Pesswani <72348929+codemike513@users.noreply.github.com>
2022-07-15 15:20:28 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      React⚛️ + Vite⚡ + Replit🌀
+      Hey Mihir Here
+    </main>
+  );
+}
+
+export default App;
